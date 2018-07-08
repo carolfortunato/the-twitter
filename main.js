01e1784af35323388f8525256dffaa8cf49f51c4
Carol Fortunato <carolfortunato@outlook.com>
2018-07-08 16:50:22 -0300

    versão 0.0.2 / 0.0.6

--- main.js
+++ main.js
@@ -1,17 +1,26 @@
-var tweet;
+document.getElementById('button').setAttribute('disabled', 'true');
+
+var tweet = document.getElementById('tweet-message');
+
+var wordCount = document.getElementById('wordCount');
+
+tweet.addEventListener('keyup', counter); // NÃO ESTÁ FUNCIONANDO
 
 function twitter() {
-  var tweet = document.getElementsByClassName('tweet-message')[0].value;
-  console.log(tweet);
+  var today = ' (posted on ' + new Date(); + ' )';
+  var tweet = document.getElementById('tweet-message').value;
   var element = document.createElement("li");
   element.appendChild(document.createTextNode(tweet));
+  element.appendChild(document.createTextNode(today));
   document.getElementById('timeline').appendChild(element);
+  document.getElementById('tweet-message').value = '';
 }
 
-/*
-tweet-message.addEventListener('keydown', counter)
-function counter () {  }
-
-*/
-
-//  PARA A DATA -> document.write(Date());
+function counter() {   // NÃO ESTÁ FUNCIONANDO
+  document.getElementById('button').removeAttribute('disabled');
+  var characters = document.getElementById('tweet-message').value.split('');
+  if (characters.length < 0 || characters.length > 140) {
+    wordCount.innerText = 140 - characters.lenght;
+  }
+  console.log(140 - characters.length);
+}
